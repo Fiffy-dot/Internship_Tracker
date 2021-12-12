bf5cf9ef7744c79ba500bd4faa64f6deedd36ff9
Fiona <f.nganga@alustudent.com>
2021-12-12 19:39:26 +0300

    employer

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.Promise = global.Promise
 app.use(bodyParser.json())
 
 // init the routes
-app.use('/api', require('./routes/api'))
+app.use('/', require('./routes/api'))
 
 app.use(function (err, req, res, next) {
   res.status(422).send({error: err.message})
@@ -21,3 +21,4 @@ app.use(function (err, req, res, next) {
 app.listen(process.env.port || 8000, () => {
   console.log('server started at http://localhost:8000');
 });
+
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const Internship = require('../models/internship')
+const Student = require('../models/student')
+const Employer = require('../models/employer')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 // get all internships
 router.get('/internships', function (req, res, next) {
@@ -40,6 +44,75 @@ router.delete('/internships/:id', function (req, res, next) {
         res.send(internship)
     }).catch(next)
 })
+
+// create an employer
+router.post('/employerRegister', async (req, res) => {
+	const { name, password: plainTextPassword, email } = req.body
+
+	if (!name || typeof name !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+    if (!email || typeof email !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid email' })
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await Employer.create({
+			name,
+			password,
+            email
+		})
+		console.log('Employer created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+
+	res.json({ status: 'ok' })
+})
+
+// get all employers
+router.get('/employers', function (req, res, next) {
+    Employer.find({}).then (function (employers) {
+        res.send(employers)
+    }).catch(next)
+})
+
+// update employer details
+router.put('/employers/:id', function (req, res, next) {
+    Employer.findByIdAndUpdate({_id: req.params.id}, req.body).
+    then (function () {
+        Employer.findOne({_id: req.params.id}).then(function (employer){
+            res.send(employer)
+        })
+    }).catch(next)
+})
+
+
+
+
+
+
+
+
+
 
 module.exports = router
 
